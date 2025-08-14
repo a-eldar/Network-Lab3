327a76ccbda7842f9f9add376940bdd12dcbdccc
amit <amit.tzur2@mail.huji.ac.il>
2025-08-14 16:59:45 +0300

    added page size calculation for buffer allocation in pp_init_ctx

--- rdma_utils.c
+++ rdma_utils.c
@@ -276,7 +276,7 @@ static struct pingpong_context *pp_init_ctx(struct ibv_device *ib_dev, int size,
     ctx->size     = size;
     ctx->rx_depth = rx_depth;
     ctx->routs    = rx_depth;
-
+    int page_size = sysconf(_SC_PAGESIZE);
     ctx->buf = malloc(roundup(size, page_size));
     if (!ctx->buf) {
         fprintf(stderr, "Couldn't allocate work buf.\n");
