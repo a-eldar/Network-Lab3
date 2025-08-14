ef0cd70b9d68f305e5403bc85ed09fb50ef83681
amit <amit.tzur2@mail.huji.ac.il>
2025-08-14 19:46:05 +0300

    commented some weird behavor

--- pg_connect.c
+++ pg_connect.c
@@ -163,15 +163,15 @@ int connect_process_group(char** serverlist, int len, int idx, PGHandle* pg_hand
     pg_handle->left_conn = pp_init_ctx(ib_dev, RDMA_BUFFER_SIZE, 10, 100, ib_port, 0, 1);
     if (!pg_handle->left_conn) {
         fprintf(stderr, "Failed to initialize left context\n");
-        pp_close_ctx(pg_handle->right_conn);
+        //pp_close_ctx(pg_handle->right_conn);
         ibv_free_device_list(dev_list);
         return -1;
     }
     pg_handle->left_conn->routs = pp_post_recv(pg_handle->left_conn, pg_handle->left_conn->rx_depth);
     if(pg_handle->left_conn->routs < pg_handle->left_conn->rx_depth) {
         fprintf(stderr, "Couldn't post receive (%d)\n", pg_handle->left_conn->routs);
-        pp_close_ctx(pg_handle->left_conn);
-        pp_close_ctx(pg_handle->right_conn);
+        // pp_close_ctx(pg_handle->left_conn);
+        // pp_close_ctx(pg_handle->right_conn);
         ibv_free_device_list(dev_list);
         return -1;
     }
