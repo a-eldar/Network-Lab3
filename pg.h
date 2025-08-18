039892692a7096f0b7b2ddd0f484e1d717f84aca
amit <amit.tzur2@mail.huji.ac.il>
2025-08-18 15:26:13 +0300

    “removed

--- pg.h
+++ pg.h
@@ -13,4 +13,4 @@ int connect_process_group(const char *host_list, int myrank, void **pg_handle);
 int pg_all_reduce(void *sendbuf, void *recvbuf, int count, int datatype_bytes, int myrank, void *pg_handle);
 int pg_close(void *pg_handle);
 
-#endif /* PG_H */
+#endif /* PG_H */ 
